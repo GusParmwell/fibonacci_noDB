9fe99be6055bcb6e0115d153089a02766af74c76
Popov <andrej.popov@accenture.com>
2018-05-28 20:02:17 +0200

    Third commit

--- test/app.js
+++ test/app.js
@@ -1,40 +1,46 @@
-const expect = require('chai').expect;
-const application = require('../app');
+const { expect } = require('chai');
+const { assert } = require('chai');
+const request = require('request')
 
 describe('IBM Problem Solution', () => {
-  describe('Fibonacci sequence', () => {
-    it('returns four numbers of fibonacci sequence starting at position taken as parameter', () => {
-      const First = application.Fibonacci(1);
-      const Second = application.Fibonacci(2);
-      const Third = application.Fibonacci(3);
+  describe('Calculates proper answer and returns it via web API', () => {
 
-      expect(First).to.equal([1, 1, 2, 3]);
-      expect(Second).to.equal([1, 2, 3, 5]);
-      expect(Third).to.equal([2, 3, 5, 8]);
+    const url = 'http://localhost:3000/sum/';
+
+    it('returns status 200', () => {
+      request(url + '3', (error, response, body) => {
+        assert(response.statusCode,200);
+      });
     });
-  });
 
-  describe('Prime numbers', () => {
-    it('returns all prime numbers smaller than or equal to input parameter', () => {
-      const First = application.Primes(1);
-      const Second = application.Primes(2);
-      const Third = application.Primes(3);
-
-      expect(First).to.equal([]);
-      expect(Second).to.equal([2]);
-      expect(Third).to.equal([2, 3]);
+    it('returns status 200', () => {
+      request(url + '4', (error, response, body) => {
+        assert(response.statusCode,200);
+      });
     });
-  });
 
-  describe('Deconstruction', () => {
-    it('deconstructs parameter using primes and returns sum of calculated multiplicands', () => {
-      const First = application.Deconstruct(3);
-      const Second = application.Deconstruct(5);
-      const Third = application.Deconstruct(8);
+    it('returns status 200', () => {
+      request(url + '5', (error, response, body) => {
+        assert(response.statusCode,200);
+      });
+    });
 
-      expect(First).to.equal(3);
-      expect(Second).to.equal(11);
-      expect(Third).to.equal(49);
+    it('returns proper answer in JSON format', () => {
+      request(url + '3', (error, response, body) => {
+        assert(body,'{"input":3, "result":[2,3,11,49]}');
+      });
+    });
+
+    it('returns proper answer in JSON format', () => {
+      request(url + '4', (error, response, body) => {
+        assert(body,'{"input":4,"result":[3,11,49,603]}');
+      });
+    });
+
+    it('returns proper answer in JSON format', () => {
+      request(url + '5', (error, response, body) => {
+        assert(body,'{"input":5,"result":[11,49,603,25681]}');
+      });
     });
   });
 });
